1c0b6b735448fb8259fe7d2b1c4501437db57149
UTP <utp@crie.local>
2017-02-27 10:32:50 -0500

    W4, w.s 1

--- taller_3/taller_3.cpp
+++ taller_3/taller_3.cpp
@@ -80,21 +80,84 @@ else{
   cout<<"Precio final del lote: "<<"$"<<p_lote<<endl<<endl;
 }
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-// ENTRADAS Horas, minutos y segundos.
-// SALIDAS Precio de la llamda en segundos.
-int horas, minutos, segundos;
-cout<<"Horas los minutos y segundos de la llamada"<<endl;
+// ENTRADAS Horas, minutos segundos cuando inició la llamada. Duracion de la llamada y día en que fue realizada.
+// SALIDAS Hora de salida, costo de la llamada en segundos.
+int horas, minutos, segundos, dia, duracion;
+float costo; 
+cout<<"Hora de inicio de la llamada (Horas, minutos y segundos)"<<endl;
 cin>>horas>>minutos>>segundos;
+int sec_entrada=(horas*3600)+(minutos*60)+segundos;
+cout<<"tiempo al aire"<<endl;
+cin>>duracion;
+int sec_salida=sec_entrada+duracion;
 cout<<"Que dia de la semana fue? "<<endl;
-if 
-cout<<"El precio de la llamada fue de: "<<<<
+cout<<"[1] Lunes\n[2] Martes\n[3] Miercoles\n[4] Jueves\n[5] Viernes\n[6] Sabado\n[7] Domingo\n";
+cin>>dia;
+cout<<"Hora de salida: "<<sec_salida/3600<<".H."<<(sec_salida%3600)/60<<".M."<<sec_salida%60<<".S."<<endl;
+if(dia>=1&&dia<=5){
+	costo=duracion*3;
+}
+else{
+	costo=duracion*2;
+}
+cout<<"El costo fue de: "<<"($"<<costo<<")"<<endl;
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 // ENTRADAS Numero de cuatro cifras
 // SALIDAS El mismo numero pero invertido
 // CONDICIONES
 int num;
-cout<<"Ingrese numero de cuatro cifras"<<endl;
-
+cout<<"\n\n\nIngrese numero de cuatro cifras"<<endl;
+cin>>num;
+cout<<"el numero nvertido es igual a: "<<((num%10)*1000)+(((num/10)%10)*100)+(((num/100)%10)*10)+(num/1000)<<"\n\n\n";
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// ENTRADAS Codigo de tres digitos
+// SALIDAS Nombre del programa asociado
+// CONDICIONES
+int cod;
+cout<<"Codigo del programa\n";
+cin>>cod;
+switch(cod){
+	case 123:
+		cout<<"Turbo C"<<endl;
+		break;
+	case 234:
+		cout<<"Word"<<endl;
+		break;
+	case 345:
+		cout<<"Power Point"<<endl;
+		break;
+	case 456:
+		cout<<"Excel"<<endl;
+		break;
+	case 567:
+		cout<<"AutoCad"<<endl;
+		break;
+	case 678:
+		cout<<"MatLab"<<endl;
+		break;
+	case 789:
+		cout<<"Corel Draw"<<endl;
+		break;
+	case 890:
+		cout<<"Otros programas"<<endl;
+		break;
+	default :
+		cout<<"Programa no identificado"<<endl<<"\n\n\n\n";
+}
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// ENTRADAS cantidad de discos cuadernos y libros seguido del precio por unidad.
+// SALIDAS Precio del pedido para pago de contado y a crédito.
+// CONDICIONES
+int libros, discos, cuadernos, plibros, pdiscos, pcuadernos;
+float totalcredito, totalcontado;
+cout<<"\n\n\n\nCantidad de libros, discos y cuadernos"<<endl;
+cin>>libros>>discos>>cuadernos;
+cout<<"Precio unitario de libros, discos y cuadernos"<<endl;
+cin>>plibros>>pdiscos>>pcuadernos;
+totalcredito= (libros*plibros)*0.80+(discos*pdiscos)*0.85+(cuadernos*pcuadernos); 
+totalcontado= ((libros*plibros)*0.80+(discos*pdiscos)*0.85+(cuadernos*pcuadernos))*0.94; 
+cout<<"Total pagando a credito: $"<<totalcredito<<endl;
+cout<<"Total pagando de contado0: $"<<totalcontado<<endl;
 
 
 
