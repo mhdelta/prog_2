86487b78f0326d2b27e04e34f776c7b8a7685154
Miguel Angel <miguelangelutp@gmail.com>
2017-02-27 15:56:15 -0500

    End w.s w5

--- Areas/triangulo.cpp
+++ Areas/triangulo.cpp
@@ -4,12 +4,12 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main() {
-	cout<<"digite la base del triangulo"<<endl;
-	int base_tri;
-	int altura_tri;
-	cin>>base_tri;
-	cout<<"digite la altura del triangulo"<<endl;
-	cin>>altura_tri;
-	cout<<"el area del triángulo es: "<<(base_tri*altura_tri/2);
-	return 0;
+cout<<"digite la base del triangulo"<<endl;
+int base_tri;
+int altura_tri;
+cin>>base_tri;
+cout<<"digite la altura del triangulo"<<endl;
+cin>>altura_tri;
+cout<<"el area del triángulo es: "<<(base_tri*altura_tri/2);
+return 0;
 }
--- taller_3/taller_3.cpp
+++ taller_3/taller_3.cpp
@@ -80,7 +80,7 @@ else{
   cout<<"Precio final del lote: "<<"$"<<p_lote<<endl<<endl;
 }
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-// ENTRADAS Horas, minutos segundos cuando inició la llamada. Duracion de la llamada y día en que fue realizada.
+// ENTRADAS Horas, minutos segundos cuando inici?la llamada. Duracion de la llamada y d? en que fue realizada.
 // SALIDAS Hora de salida, costo de la llamada en segundos.
 int horas, minutos, segundos, dia, duracion;
 float costo; 
@@ -145,8 +145,8 @@ switch(cod){
 		cout<<"Programa no identificado"<<endl<<"\n\n\n\n";
 }
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-// ENTRADAS cantidad de discos cuadernos y libros seguido del precio por unidad.
-// SALIDAS Precio del pedido para pago de contado y a crédito.
+// ENTRADAS Cantidad de discos cuadernos y libros seguido del precio por unidad.
+// SALIDAS Precio del pedido para pago de contado y a credito.
 // CONDICIONES
 int libros, discos, cuadernos, plibros, pdiscos, pcuadernos;
 float totalcredito, totalcontado;
@@ -157,9 +157,117 @@ cin>>plibros>>pdiscos>>pcuadernos;
 totalcredito= (libros*plibros)*0.80+(discos*pdiscos)*0.85+(cuadernos*pcuadernos); 
 totalcontado= ((libros*plibros)*0.80+(discos*pdiscos)*0.85+(cuadernos*pcuadernos))*0.94; 
 cout<<"Total pagando a credito: $"<<totalcredito<<endl;
-cout<<"Total pagando de contado0: $"<<totalcontado<<endl;
+cout<<"Total pagando de contado: $"<<totalcontado<<endl;
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// ENTRADAS Temperatura y escala a la que se va a convertir
+// SALIDAS Equivalencias en las otras escalas
+// CONDICIONES
+cout<<"PTemperaturas"<<endl;
+    cout<<"Transformar"<<endl;
+    cout<<"\n[1] Celsius"<<endl;
+    cout<<"[2] Fahrenheit"<<endl;
+    cout<<"[3] Kelvin"<<endl;
+
+int seleccion; cin>>seleccion;
+cout<<"Valor a transformar: "<<endl;
+float grados ; cin>>grados;
+
+switch (seleccion){
+    case 1:
+    cout<<grados<<" grados celsius equivalen a "<<endl;
+    cout<<(((9/5)*grados)+32)<<" grados Fahrenheit"<<endl;
+    cout<<(grados+273.15)<<" grados Kelvin"<<endl;
+    break;
+
+    case 2:
+    cout<<grados<<" grados Fahrenheit equivalen a "<<endl;
+    cout<<(((5/9)*grados)-32)<<" grados Celsius"<<endl;
+    cout<<(grados-273.15)<<" grados Kelvin"<<endl;
+    break;
+
+    case 3:
+    cout<<grados<<" grados Kelvin equivalen a "<<endl;
+    cout<<(((9/5)*grados)-459.67)<<" grados Fahrenheit"<<endl;
+    cout<<(grados-273.15)<<" grados celsius"<<endl;
+    break;
+}
+cout<<""<<endl;
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// ENTRADAS Seleccion de la figura geometrica, dimensiones de la figura.
+// SALIDAS Area de la figura seleccionada
+// CONDICIONES Figuras admitidas: circulo, rectangulo o triangulo
+int sel;
+cout<<"Seleccione la figura"<<endl;
+cout<<"[1]Rectagulo\n[2]Triangulo\n[3]Circulo"<<endl;
+cin>>sel;
+if(sel==1){
+    cout<<"digite la base del rectangulo"<<endl;
+    int base_rec;
+    int altura_rec;
+    cin>>base_rec;
+    cout<<"digite la altura del rectangulo"<<endl;
+    cin>>altura_rec;
+    cout<<"el area del rectangulo es: "<<base_rec*altura_rec;
+}
+else{
+    if(sel==2){
+        cout<<"digite la base del triangulo"<<endl;
+        int base_tri;
+        int altura_tri;
+        cin>>base_tri;
+        cout<<"digite la altura del triangulo"<<endl;
+        cin>>altura_tri;
+        cout<<"el area del triÃ¡ngulo es: "<<(base_tri*altura_tri/2);
+    }
+else{
+    cout<<"digite el radio del circulo"<<endl;
+    float radio;
+    float pi=3.14159265359;
+    cin>>radio;
+    cout<<"el area del circulo es: "<<(pi*radio*radio);    
+    }
+}
 
 
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// ENTRADAS Hora de ventas, monto de las compras
+// SALIDAS Total a pagar y total de dinero ahorrado.
+// CONDICIONES
+cout<<"\n\n\nHora de la compra (separe con un . ejemplo: 12.30)"<<endl;
+float hora;
+float m_fruver, m_aseo, m_carnes, m_ropa, m_licor;
+float total;
+cin>>hora;
+cout<<"Monto Frutas y verduras"<<endl;
+cin>>m_fruver;
+cout<<"Monto Productos de aseo"<<endl;
+cin>>m_aseo;
+cout<<"Monto Carnes"<<endl;
+cin>>m_carnes;
+cout<<"Monto Ropa y calzado"<<endl;
+cin>>m_ropa;
+cout<<"Monto Licores"<<endl;
+cin>>m_licor;
+if(hora<8.30){
+    total=(m_fruver*0.9+m_licor*0.92+m_ropa*0.9+m_aseo*0.95+m_carnes*0.95);     
+}
+else{
+    if(hora<9.30){    
+        total=(m_fruver*0.95+m_licor*0.94+m_ropa*0.92+m_aseo*0.98+m_carnes*0.98);
+    }
+}
+cout<<"El total a pagar es: $"<<total<<endl;
+cout<<"El total ahorrado es de: $"<<(m_fruver*0.1+m_licor*0.08+m_ropa*0.1+m_aseo*0.05+m_carnes*0.05)<<endl;
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// ENTRADAS 
+// SALIDAS 
+// CONDICIONES
+int i, n = 20;
+for (i=0; i<n; i--)
+{
+cout << "x" << endl;
+}
 
+ 
 return 0;
 }
